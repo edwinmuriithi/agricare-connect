b1b71d649e49132cb9ca144193e8cd877f69fdcc
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-10-11 10:21:03 +0300

    Add record Layout

--- app/src/main/java/com/example/appbar/ui/farmhelp/FarmHelpRecord.java
+++ app/src/main/java/com/example/appbar/ui/farmhelp/FarmHelpRecord.java
@@ -1,8 +1,10 @@
-package com.example.appbar;
+package com.example.appbar.ui.farmhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.appbar.R;
 
 public class FarmHelpRecord extends AppCompatActivity {
 
