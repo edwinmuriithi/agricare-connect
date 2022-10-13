2bbdbd9b4434f92b2640db65b9ec17946eac61a2
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-10-13 10:18:09 +0300

    Add Farm Video Web View

--- app/src/main/java/com/example/appbar/ui/farmvideos/FarmVideoFragment.java
+++ app/src/main/java/com/example/appbar/ui/farmvideos/FarmVideoFragment.java
@@ -1,9 +1,13 @@
 package com.example.appbar.ui.farmvideos;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +16,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.appbar.databinding.FragmentFarmVideoBinding;
+
+import javax.security.auth.callback.Callback;
 
 public class FarmVideoFragment extends Fragment {
 
@@ -29,10 +35,19 @@ public class FarmVideoFragment extends Fragment {
 
         //Disable darkmode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
+        WebSettings webSettings = binding.farmVideo.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        binding.farmVideo.setWebViewClient(new Callback());
+        binding.farmVideo.loadUrl("https://www.youtube.com/user/beeflambnz/videos");
 
         return root;
     }
+        private  class Callback extends WebViewClient{
+            @Override
+            public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+                return false;
+            }
+        }
 
     @Override
     public void onDestroyView() {
