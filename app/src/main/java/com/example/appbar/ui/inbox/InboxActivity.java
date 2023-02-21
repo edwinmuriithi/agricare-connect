c6e769329d1f91090d8fc3f130ed36c794dce7eb
edwinmuriithi <kabuimuriithi@gmail.com>
2023-02-21 17:11:09 +0300

    feat(WebSockets): Create websockets

--- app/src/main/java/com/example/appbar/ui/inbox/InboxActivity.java
+++ app/src/main/java/com/example/appbar/ui/inbox/InboxActivity.java
@@ -1,6 +1,8 @@
 package com.example.appbar.ui.inbox;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
@@ -10,21 +12,49 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.appbar.R;
 import com.example.appbar.databinding.ActivityFarmVideoBinding;
 import com.example.appbar.databinding.ActivityInboxBinding;
+import com.example.appbar.model.UserDetails;
+import com.example.appbar.storage.SharedPreferencesManager;
 import com.example.appbar.ui.farmhelp.FarmHelp;
 import com.example.appbar.ui.farmvideos.FarmVideo;
 import com.example.appbar.ui.home.HomeActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class InboxActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Base64;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+
+public class InboxActivity extends AppCompatActivity implements TextWatcher {
 
     private ActivityInboxBinding binding;
+    private WebSocket webSocket;
+    private String name;
+    private String SERVER_PATH = "";
+    private int GALLERY_REQUEST_CODE = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +63,10 @@ public class InboxActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+//        UserDetails userDetails = SharedPreferencesManager.getInstance(this).getUser();
+        initiateSocketConnection();
+
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.nav_view);
@@ -67,5 +101,132 @@ public class InboxActivity extends AppCompatActivity {
                 return false;
 
         });
+    }
+
+    private void initiateSocketConnection() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(SERVER_PATH).build();
+        webSocket = client.newWebSocket(request,new SocketListener());
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String string = s.toString().trim();
+        if (string.isEmpty()){
+            resetMessageEdit();
+        }else {
+            binding.sendBtn.setVisibility(View.VISIBLE);
+            binding.pickImgBtn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void resetMessageEdit() {
+        binding.messageEdit.removeTextChangedListener(this);
+
+        binding.messageEdit.setText("");
+        binding.sendBtn.setVisibility(View.INVISIBLE);
+        binding.pickImgBtn.setVisibility(View.VISIBLE);
+
+        binding.messageEdit.addTextChangedListener(this);
+    }
+
+    private class SocketListener extends WebSocketListener {
+        @Override
+        public void onMessage(@NonNull WebSocket webSocket, @NonNull String text) {
+            super.onMessage(webSocket, text);
+
+            runOnUiThread(() -> {
+                try {
+                    JSONObject jsonObject = new JSONObject(text);
+                    jsonObject.put("isSent",false);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        @Override
+        public void onOpen(@NonNull WebSocket webSocket, @NonNull Response response) {
+            super.onOpen(webSocket, response);
+
+            runOnUiThread(() -> {
+                Toast.makeText(InboxActivity.this, "Socket Connection Successful", Toast.LENGTH_SHORT).show();
+                initializeView();
+            });
+        }
+    }
+
+    private void initializeView() {
+        binding.messageEdit.addTextChangedListener(this);
+
+        binding.sendBtn.setOnClickListener(v -> {
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("text",binding.messageEdit.getText().toString());
+
+                webSocket.send(jsonObject.toString());
+
+                jsonObject.put("isSent", true);
+
+                resetMessageEdit();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+
+        binding.pickImgBtn.setOnClickListener(v ->{
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+
+            startActivityForResult(Intent.createChooser(intent,"Pick Image"),GALLERY_REQUEST_CODE);
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
+            try {
+                InputStream is = getContentResolver().openInputStream(data.getData());
+                Bitmap image = BitmapFactory.decodeStream(is);
+
+                sendImage(image);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void sendImage(Bitmap image) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG,50,outputStream);
+
+//        String base64String = Base64.getEncoder().encodeToString(outputStream.toByteArray(),Base64.getEncoder());
+        JSONObject jsonObject = new JSONObject();
+        try {
+
+//        jsonObject.put("image", base64String);
+
+            webSocket.send(jsonObject.toString());
+
+            jsonObject.put("isSent", true);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 }
